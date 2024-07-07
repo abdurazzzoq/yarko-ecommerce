00b2933a866100af52d4a3bbfd42763938d6025f
abdurazzzoq <abdurazzoqabduhakimov1@gmail.com>
2024-07-07 21:09:11 +0200

    bugs are fixed

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -4,19 +4,18 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-const [product, setProduct] = useState(null);
-const router = useRouter();
-const { id } = router.query;
-useEffect(() => {
-  if (!id) {
-    return;
-  }
-  axios.get("/api/products?id=" + id).then((res) => {
-    setProduct(res.data);
-  });
-}, [id]);
-
 export default function editProduct() {
+  const [product, setProduct] = useState(null);
+  const router = useRouter();
+  const { id } = router.query;
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    axios.get("/api/products?id=" + id).then((res) => {
+      setProduct(res.data);
+    });
+  }, [id]);
   return (
     <Layout>
       <h1>Mahsulotni Ozgartirish</h1>
