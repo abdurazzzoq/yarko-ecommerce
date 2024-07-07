5598099fd293d10f2de158a2bd45c9b2149d33d5
abdurazzzoq <abdurazzoqabduhakimov1@gmail.com>
2024-07-07 21:28:18 +0200

    fixed

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -1,25 +1,27 @@
 import Layout from "@/components/Layout";
-import ProductForm from "@/components/ProductForm";
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
-export default function editProduct() {
-  const [product, setProduct] = useState(null);
+export default function EditProductPage() {
+  const [productInfo, setProductInfo] = useState(null);
   const router = useRouter();
-  const { id } = router.query;
+  const {id} = router.query;
   useEffect(() => {
     if (!id) {
       return;
     }
-    axios.get("/api/products?id=" + id).then((res) => {
-      setProduct(res.data);
+    axios.get('/api/products?id='+id).then(response => {
+      setProductInfo(response.data);
     });
   }, [id]);
   return (
     <Layout>
-      <h1>Mahsulotni Ozgartirish</h1>
-      {product && <ProductForm {...product} />}
+      <h1>Edit product</h1>
+      {productInfo && (
+        <ProductForm {...productInfo} />
+      )}
     </Layout>
   );
 }
